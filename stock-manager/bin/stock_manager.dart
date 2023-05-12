b2a71347baa8f5b91de8d3607a4c48466345ef00
Aline <alinedsb2016@gmail.com>
2023-05-12 20:49:05 -0300

    criação do projeto backend

---
+++ stock-manager/bin/stock_manager.dart
@@ -0,0 +1,5 @@
+import 'package:stock_manager/stock_manager.dart' as stock_manager;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stock_manager.calculate()}!');
+}
---
+++ stock-manager/test/stock_manager_test.dart
@@ -0,0 +1,8 @@
+import 'package:stock_manager/stock_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
